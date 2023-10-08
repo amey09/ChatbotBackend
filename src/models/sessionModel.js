3dd4c323bbaf33d0751b8851a558b5903c651e18
amey09 <ameymhatre09@gmail.com>
2023-10-08 22:17:53 +0530

    feat: add user and session schemas for mongodb

---
+++ src/models/sessionModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const sessionSchema = mongoose.Schema({
+  uuid: {
+    type: String,
+    default: undefined,
+  },
+  wardenID: {
+    type: String,
+    required: true,
+  },
+  wardenName: {
+    type: String,
+  },
+  datetime: {
+    type: Date,
+    required: true,
+  },
+  mode: {
+    type: String,
+  },
+});
+
+const Session = mongoose.model("Session", sessionSchema);
+
+export default Session;
